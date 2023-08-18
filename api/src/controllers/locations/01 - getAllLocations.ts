a6063441870562be6997d9c226d1c793b2f7138e
JoseBritoP <josealbritose@gmail.com>
2023-08-18 17:49:43 -0300

    Type

--- api/src/controllers/locations/01 - getAllLocations.ts
+++ api/src/controllers/locations/01 - getAllLocations.ts
@@ -1,6 +1,6 @@
 import { Location } from "../../db/db";
-
+import { getAllLocationsApi } from "./02 - getAllLocationsApi";
 export const getAllLocations = async () => {
-
+  const locationsApi = await getAllLocationsApi();
   
 };
--- api/src/helpers/location/cleanArray.ts
+++ api/src/helpers/location/cleanArray.ts
@@ -1,6 +1,7 @@
+import { LocationInterface } from "../../interfaces/location";
 export const cleanArrayApi = (locations:any) =>{
   // console.log(locations)
-  return locations.map((location:any)=>({
+  return locations.map((location:LocationInterface)=>({
     id: location.id,
     name : location.name,
     type: location.type,
