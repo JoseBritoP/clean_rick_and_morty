b0a41f04a1354a694e2aeb59210ac310f24e9f9f
JoseBritoP <josealbritose@gmail.com>
2023-08-19 02:32:13 -0300

    get All Location

--- api/src/controllers/locations/01 - getAllLocations.ts
+++ api/src/controllers/locations/01 - getAllLocations.ts
@@ -1,6 +1,29 @@
 import { Location } from "../../db/db";
 import { getAllLocationsApi } from "./02 - getAllLocationsApi";
+
 export const getAllLocations = async () => {
-  const locationsApi = await getAllLocationsApi();
-  
+
+  const locations = await Location.findAll();
+  if(locations.length === 0){
+
+    const locationsApi = await getAllLocationsApi();
+    
+    const creationPromises = locationsApi.map((location) =>
+    Location.findOrCreate({
+      where: {
+        id: location.id,
+        name: location.name,
+        type: location.type,
+        dimension: location.dimension,
+      },
+    })
+    );
+    
+    await Promise.all(creationPromises);
+    
+    const allLocations = await Location.findAll();
+    return allLocations;
+  }
+
+  return locations;
 };
--- api/src/handlers/locationsHandler.ts
+++ api/src/handlers/locationsHandler.ts
@@ -1,10 +1,11 @@
 import { Request,Response } from "express";
 // import { ErrorRequestHandler } from "express";
 import { getAllLocationsApi } from "../controllers/locations/02 - getAllLocationsApi";
+import { getAllLocations } from "../controllers/locations/01 - getAllLocations";
 
 export const getLocations = async (_req:Request,res:Response) => {
   try {
-    const locations = await getAllLocationsApi();
+    const locations = await getAllLocations();
     // return res.status(200).json({DIY:'All Locations'})
     return res.status(200).json(locations)
   } catch (error:any) {
