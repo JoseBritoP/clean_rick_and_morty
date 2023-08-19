d503bb1a6263de5879b11b2ba148c88e97a12d5f
JoseBritoP <josealbritose@gmail.com>
2023-08-19 00:59:37 -0300

    Testing Location

---
+++ api/src/tests/locations.test.ts
@@ -0,0 +1,37 @@
+import { server } from '../index';
+import request from 'supertest';
+// import { LocationInterface } from '../interfaces/location';
+export interface LocationInterface {
+  id:number,
+  name:string;
+  type: string,
+  dimension: string,
+}
+
+describe('/GET Location',()=>{
+  test('should respond with a 200 status code',async()=>{
+    const response: request.Response = await request(server).get('/location');
+    expect(response.status).toBe(200);
+    expect(response.ok).toBe(true)
+  });
+  // it ('should return an array of locations', async () => {
+  test('should return an array of locations',async()=>{
+    const response: request.Response = await request(server).get('/location');
+    // console.log(response.body)
+    expect(response.body).toBeInstanceOf(Array);
+  });
+  test('should return an array of locations with instance contract',async()=>{
+    const response:request.Response = await request(server).get('/location');
+    const locations: LocationInterface[] = response.body;
+
+    locations.forEach((location) => {
+      expect(typeof location).toBe('object');
+      expect(location).toMatchObject<LocationInterface>({
+        id: expect.any(Number),
+        name: expect.any(String),
+        type: expect.any(String),
+        dimension: expect.any(String),
+      });
+    });
+  });
+});
