116c11d315a4e8d5b646354c659de26ebaf2cd4c
JoseBritoP <josealbritose@gmail.com>
2023-08-19 02:46:47 -0300

    Location Single

--- api/src/controllers/locations/01 - getAllLocations.ts
+++ api/src/controllers/locations/01 - getAllLocations.ts
@@ -1,29 +1,27 @@
 import { Location } from "../../db/db";
 import { getAllLocationsApi } from "./02 - getAllLocationsApi";
 
-export const getAllLocations = async () => {
+export const savingLocationsBDD = async () => {
+  const locationsApi = await getAllLocationsApi();
+    
+  const creationPromises = locationsApi.map((location) => Location.findOrCreate({
+    where: {
+      id: location.id,
+      name: location.name,
+      type: location.type,
+      dimension: location.dimension,
+    },
+  }));
+  
+  return await Promise.all(creationPromises);
+};
 
+export const getAllLocations = async () => {
   const locations = await Location.findAll();
   if(locations.length === 0){
-
-    const locationsApi = await getAllLocationsApi();
-    
-    const creationPromises = locationsApi.map((location) =>
-    Location.findOrCreate({
-      where: {
-        id: location.id,
-        name: location.name,
-        type: location.type,
-        dimension: location.dimension,
-      },
-    })
-    );
-    
-    await Promise.all(creationPromises);
-    
+    await savingLocationsBDD();
     const allLocations = await Location.findAll();
     return allLocations;
   }
-
   return locations;
 };
