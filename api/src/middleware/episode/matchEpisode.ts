159df839ce3179beac21200aa0a54d6029298a80
JoseBritoP <josealbritose@gmail.com>
2023-08-20 15:30:40 -0300

    Episodes test

--- api/src/middleware/episode/matchEpisode.ts
+++ api/src/middleware/episode/matchEpisode.ts
@@ -9,14 +9,7 @@ export const matchEpisode = async (req:Request,res:Response,next:NextFunction) =
         name:name
       }
     });
-    const episodeExistInSession  = await Episode.findOne({
-      where:{
-        episode:episode
-      }
-    });
-    if(episodeExist && episodeExistInSession) throw new Error(`There is already exist the episode named ${name} in  ${episode}`);
     if(episodeExist) throw new Error(`There is already exist a episode name: ${name}`);
-    if(episodeExistInSession) throw new Error(`There is already exist a episode ${episode}`)
     next();
   } catch (error:any) {
     return res.status(400).json({error: error.message});
--- api/src/tests/episodes.test.ts
+++ api/src/tests/episodes.test.ts
@@ -74,3 +74,168 @@ describe('GET /episode/:id',()=>{
     })
   });
 });
+
+describe('POST /episode',()=>{
+  describe('Success case',()=>{
+    test('should respond with a 201 status code',async()=>{
+      const newEpisodeTest = {
+        name: 'new episode Test',
+        air_date: 'new episode Test',
+        episode: 'new episode Test',
+      };
+      const response = await request(server).post('/episode').send(newEpisodeTest);
+      expect(response.status).toBe(201);
+      expect(response.ok).toBeTruthy();
+      expect(response.ok).toBe(true);
+    });
+
+    test('should respond with a content-type of application/json in header',async()=>{
+      const newEpisodeTest = {
+        name: 'new episode Test two',
+        air_date: 'new episode Test',
+        episode: 'new episode Test',
+      };
+      const response = await request(server).post('/episode').send(newEpisodeTest);
+      expect(response.header['content-type']).toEqual(expect.stringContaining('json'));
+    });
+
+    test('should respond with a json object containing the new episode on id',async()=>{
+      const newEpisodeTest = {
+        name: 'new episode Test three',
+        air_date: 'new episode Test',
+        episode: 'new episode Test',
+      };
+
+      const response = await request(server).post('/episode').send(newEpisodeTest);
+      expect(response.body.id).toBeDefined();
+    });
+    test('should respond with a json object equality episode interface',async()=>{
+      const newEpisodeTest = {
+        name: 'new episode Test four',
+        air_date: 'new episode Test',
+        episode: 'new episode Test',
+        } as EpisodeInterface;
+        const response = await request(server).post('/episode').send(newEpisodeTest);
+        expect(response.body).toMatchObject<EpisodeInterface>({
+          id: expect.any(Number),
+          name: expect.any(String),
+          air_date: expect.any(String),
+          episode: expect.any(String),
+        });
+    });
+  });
+  describe('Error case',()=>{
+    test('should respond with a status 400 code if is missing data',async()=>{
+      const fields = [
+        {},
+        {name:'Test episode'},
+        {air_date:'Test air_date'},
+        {episode: 'Test episode'},
+        {name:'Test episode',air_date:'Test air_date'},
+        {name:'Test episode',episode:"Test episode"},
+        {air_date:'Test air_date',episode:'Test episode'}
+      ];
+
+      for (const body of fields){
+        const response = await request(server).post('/episode').send(body);
+        expect(response.status).toBe(400);
+        expect(response.clientError).toBe(true);
+      }
+    });
+    test('should respond with a message error if is missing data',async()=>{
+      const episodeTest = [
+        {
+          name: 1,
+          air_date :'testair_date',
+          episode :"testepisode"
+        },
+        {
+          name:'Test episode',
+          air_date :1,
+          episode :"testepisode"
+        },
+        {
+          name:'Test episode',
+          air_date :'testair_date',
+          episode : 0
+        },
+      ]
+
+      const episodeTesting = episodeTest.map(async(episode)=>{
+        const response = await request(server).post('/episode').send(episode);
+        expect(response.status).toBe(400);
+        expect(response.clientError).toBe(true);
+      });
+      await Promise.all(episodeTesting);
+    });
+    test('should respond with a status 400 if the prop air_date is invalid',async()=>{
+      const episodeTest = [
+        {
+          name: 1,
+          air_date :'testair_date',
+          episode :"testepisode"
+        },
+        {
+          name:'Test episode',
+          air_date :1,
+          episode :"testepisode"
+        },
+        {
+          name:'Test episode',
+          air_date :'testair_date',
+          episode : 0
+        },
+      ]
+
+      const episodeTesting = episodeTest.map(async(episode)=>{
+        const response = await request(server).post('/episode').send(episode);
+        expect(response.body.error).toMatchObject({
+          issues: expect.any(Array),
+          name: 'ZodError'
+        });
+      })
+      await Promise.all(episodeTesting)
+    });
+    test('should respond with a message error for invalidate prop air_date',async()=>{
+      const episodeTest = [
+        {
+          name: 1,
+          air_date :'testair_date',
+          episode :"testepisode"
+        },
+        {
+          name:'Test episode',
+          air_date :1,
+          episode :"testepisode"
+        },
+        {
+          name:'Test episode',
+          air_date :'testair_date',
+          episode : 0
+        },
+      ]
+
+      const episodeTesting = episodeTest.map(async(episode)=>{
+        const response = await request(server).post('/episode').send(episode);
+        expect(response.body.error).toMatchObject({
+          issues: expect.any(Array),
+          name: 'ZodError'
+        });
+      })
+      await Promise.all(episodeTesting)
+    });
+    test('should respond with a message error in case the episode name already exist',async()=>{
+      const episodeTest = {
+        name: "Pilot",
+        air_date: "December 2, 2013",
+        episode: "S02E10"
+      };
+
+      const response = await request(server).post('/episode').send(episodeTest);
+      expect(response.status).toBe(400);
+      expect(response.clientError).toBe(true);
+      // console.log(response.body.error)
+      expect(response.body.error).toBe(`There is already exist a episode name: ${episodeTest.name}`)
+    });
+  });
+});
